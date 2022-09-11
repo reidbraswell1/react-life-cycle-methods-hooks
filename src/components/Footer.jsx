877a42cfcd09e32e2572eccf0b34a5553503e373
reid <reidbras@gmail.com>
2022-09-10 20:46:01 -0500

    Adding Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function Footer(props) {
+
+    const [ footerText, setFooterText ] = useState("Life Cycle Methods Exercise 9/22");
+    return (
+        <div className="row mt-3">
+            <footer className="col-5 my-center footer">
+                <p>{footerText}</p>
+            </footer>
+        </div>);
+}
+export default Footer;
