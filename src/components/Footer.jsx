a101997a75a9891bc6fda4853f83ce724254c308
reid <reidbras@gmail.com>
2022-09-12 12:52:45 -0500

    Updating footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 function Footer(props) {
 
-    const [ footerText, setFooterText ] = useState("Life Cycle Methods Exercise 9/22");
+    const [ footerText, setFooterText ] = useState("Life Cycle Methods Exercise Hooks 9/22");
     return (
         <div className="row mt-3">
             <footer className="col-5 my-center footer">
